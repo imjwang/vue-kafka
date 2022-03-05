fe2e157dacf2bdfb2668d4b27f7bc671c9ab6d4b
imjwang <ngjaywa@gmail.com>
2022-03-05 01:29:59 -0500

    feat: drag and resize graphcard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { useWebSocket, useTimeout } from '@vueuse/core'
+import { useWebSocket, useTimeout, useDraggable } from '@vueuse/core'
 import { watch, ref, provide } from 'vue'
 import { defineStore } from 'pinia'
 import BarChart from '@/components/BarChart.vue'
@@ -20,16 +20,34 @@ const test = ref([{
   data: val.value
 }])
 
-const test2 = ref({
-        chart: {
-          id: 'vuechart-example'
-        },
-        xaxis: {
-          categories: val.value.length
-        }
-      })
-
-
+const chartOptions = {
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'straight'
+            },
+            title: {
+              text: 'My Graph',
+              align: 'left'
+            },
+            grid: {
+              row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+              },
+            },
+            xaxis: {
+              categories: val.value.length,
+            }
+          }
 
 watch(data, (newData) => {
   const parse = JSON.parse(newData)
@@ -39,6 +57,8 @@ watch(data, (newData) => {
 //provide kafka data
 provide('data', val)
 
+const retest = ref(null)
+
 </script>
 
 <template>
@@ -46,12 +66,8 @@ provide('data', val)
   <Nav />
   <h1 class="text-4xl">Data Testing</h1>
   <p>{{val[val.length-1] || 'No Data Received Yet.'}}</p>
-    <div class="flex flex-col w-full h-5/6 lg:flex-row">
     <ChartCard>
-      <apexchart width="500" :options=test2 type="line" :series="test"></apexchart>
+      <apexchart ref="retest" width="100%" height="100%" :options=chartOptions type="line" :series="test"></apexchart>
     </ChartCard>
-        <div class="pt-8 divider lg:divider-horizontal"></div>
-    <ChartCard />
-    </div>
 </div>
 </template>
--- frontend/src/components/ChartCard.vue
+++ frontend/src/components/ChartCard.vue
@@ -1,15 +1,25 @@
 <script setup>
 //TODO use title prop and description
 //TODO slot for placing widgets
+import { ref } from 'vue'
+import { useDraggable, useResizeObserver, debouncedWatch } from '@vueuse/core'
 
+
+const drag = ref(null)
+
+const { x, y, style } = useDraggable(drag, {
+  onStart: (pos, e) => {
+    return e.ctrlKey
+  },
+  initialValue: { x: 40, y: 40 },
+})
 </script>
 
 
 
  <template>
-      <div class="card border-2 border-info w-full h-fit text-info-content my-4 mx-4">
-      <div class="card-body items-center">
+      <div ref="drag" :style="style" style="position: fixed" class="hover:shadow-md hover:shadow-info resize card border-2
+       border-info w-min h-min text-info-content my-4 mx-4 py-2 px-2">
         <slot></slot>
-      </div>
-      </div>     
+      </div>  
  </template>
